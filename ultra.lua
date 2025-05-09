2a95a42ddcdf5b2a6ebbeed1b35296a03ebc02fc
Alskjsjsjs <amunanay@gmail.com>
2025-05-09 18:56:24 +0700

    ultra.lua

--- ultra.lua
+++ ultra.lua
@@ -1,42 +1,4 @@
--- SISTEM KEY DULU
-local allowedKey = "Alimkey"
-
--- Buat GUI input key
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local TextBox = Instance.new("TextBox")
-local SubmitButton = Instance.new("TextButton")
-local UICorner = Instance.new("UICorner")
-
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.ResetOnSpawn = false
-
-Frame.Size = UDim2.new(0, 300, 0, 150)
-Frame.Position = UDim2.new(0.5, -150, 0.5, -75)
-Frame.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-Frame.Parent = ScreenGui
-UICorner.Parent = Frame
-
-TextBox.PlaceholderText = "Masukkan Key..."
-TextBox.Size = UDim2.new(0, 260, 0, 50)
-TextBox.Position = UDim2.new(0, 20, 0, 20)
-TextBox.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextBox.Parent = Frame
-
-SubmitButton.Text = "Submit"
-SubmitButton.Size = UDim2.new(0, 260, 0, 40)
-SubmitButton.Position = UDim2.new(0, 20, 0, 90)
-SubmitButton.BackgroundColor3 = Color3.fromRGB(70, 130, 180)
-SubmitButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-SubmitButton.Parent = Frame
-
-SubmitButton.MouseButton1Click:Connect(function()
-	if TextBox.Text == allowedKey then
-		ScreenGui:Destroy() -- Hapus UI key
-
-		-- SCRIPT GUI UTAMA MULAI DI SINI
-		divine_flying = nil
+divine_flying = nil
 
 if workspace:FindFirstChild("divine_flying") == nil then
 divine_flying = Instance.new("BoolValue")
@@ -496,4 +458,4 @@ function run()
 	cash_register.CanCollide = false  
 	local ultima_cash_register = nil  
 	while true do  
-		
+		cash_regi
